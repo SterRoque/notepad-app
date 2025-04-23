709d2f8dfb99a41f57206d25ab0b14eebcc87866
Sterphane Roque <stermiss2@gmail.com>
2025-04-23 17:13:42 -0300

    feat: add dark mode support and background color

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import MasonryList from "@react-native-seoul/masonry-list";
-import { notes } from "./src/constants/notes";
-import { Card } from "./src/components/card/card";
-import { INote } from "./src/interfaces/note-interface";
-import "./global.css";
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import MasonryList from '@react-native-seoul/masonry-list';
+import { notes } from './src/constants/notes';
+import { Card } from './src/components/card';
+import { INote } from './src/interfaces/note-interface';
+import './global.css';
+import { useColorScheme } from 'nativewind';
+import { useEffect } from 'react';
 
 export default function App() {
-  return (
-    <View className="flex-1">
-      <StatusBar style="auto" />
-      <MasonryList
-        keyExtractor={(item) => item.id}
-        style={{ gap: 10, padding: 15 }}
-        numColumns={2}
-        data={notes}
-        renderItem={({ item }) => <Card note={item as INote} />}
-      />
-    </View>
-  );
+   const { setColorScheme } = useColorScheme();
+
+   useEffect(() => {
+      setColorScheme('system');
+   }, []);
+
+   return (
+      <View className='flex-1 bg-[#f7f7f7] dark:bg-black'>
+         <StatusBar style='auto' />
+         <MasonryList
+            keyExtractor={(item) => item.id}
+            style={{ gap: 10, padding: 15 }}
+            numColumns={2}
+            data={notes}
+            renderItem={({ item }) => <Card note={item as INote} />}
+         />
+      </View>
+   );
 }
