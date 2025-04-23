851eac14261efa1a9d31edb561e546eee4036273
Sterphane Roque <stermiss2@gmail.com>
2025-04-23 17:13:36 -0300

    feat: add Card component for displaying notes

---
+++ src/components/card.tsx
@@ -0,0 +1,22 @@
+import { Text, TouchableOpacity } from 'react-native';
+import { INote } from '../interfaces/note-interface';
+import { truncateText } from '../utils/truncate-text';
+import { formatDate } from '../utils/formate-date';
+
+type CardProps = {
+   note: INote;
+};
+
+export function Card({ note }: CardProps) {
+   return (
+      <TouchableOpacity className='mb-[10px] gap-5 rounded-xl bg-white p-8 dark:bg-[#242424]'>
+         <Text className='text-[18px] font-bold dark:text-white'>
+            {truncateText(note.title)}
+         </Text>
+         <Text className='text-[14px] text-[#7e7e7e]'>{note.description}</Text>
+         <Text className='text-[11px] text-[#7e7e7e]'>
+            {formatDate(note.createdAt)}
+         </Text>
+      </TouchableOpacity>
+   );
+}
