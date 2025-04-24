6b3b1a109ee32ccb6a5c48e4a564760c641a1f30
Sterphane Roque <stermiss2@gmail.com>
2025-04-23 22:32:39 -0300

    feat: add input component to HomeScreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,15 @@ import { View } from 'react-native';
 import { notes } from '../constants/notes';
 import { Card } from '../components/card';
 import { INote } from '../interfaces/note-interface';
+import { Input } from '../components/input';
 
 export default function HomeScreen() {
    return (
-      <View className='flex-1 bg-[#f7f7f7] dark:bg-black'>
+      <View className='flex-1 bg-[#f7f7f7] p-4 dark:bg-black'>
+         <Input placeholder='Procurar atonações' />
          <MasonryList
             keyExtractor={(item) => item.id}
-            style={{ gap: 10, padding: 15 }}
+            style={{ gap: 10 }}
             numColumns={2}
             data={notes}
             renderItem={({ item }) => <Card note={item as INote} />}
