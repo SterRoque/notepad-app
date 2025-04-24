4b12060b59d8943e2101d5b43386e802ef734f51
Sterphane Roque <stermiss2@gmail.com>
2025-04-23 23:03:37 -0300

    feat: add title, input wrapper, and button add component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,20 @@
 import MasonryList from '@react-native-seoul/masonry-list';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import { notes } from '../constants/notes';
 import { Card } from '../components/card';
 import { INote } from '../interfaces/note-interface';
 import { Input } from '../components/input';
+import { ButtonAdd } from '../components/button-add';
 
 export default function HomeScreen() {
    return (
       <View className='flex-1 bg-[#f7f7f7] p-4 dark:bg-black'>
-         <Input placeholder='Procurar atonações' />
+         <Text className='my-7 text-3xl font-bold dark:text-white'>
+            Anotações
+         </Text>
+         <View className='mb-5'>
+            <Input placeholder='Procurar atonações' />
+         </View>
          <MasonryList
             keyExtractor={(item) => item.id}
             style={{ gap: 10 }}
@@ -16,6 +22,7 @@ export default function HomeScreen() {
             data={notes}
             renderItem={({ item }) => <Card note={item as INote} />}
          />
+         <ButtonAdd />
       </View>
    );
 }
